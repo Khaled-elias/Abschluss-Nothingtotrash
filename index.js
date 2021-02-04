e0fe5140365031ec4a94b97345d5d000c0d949aa
Khaled-994 <kalkhaled1994@gmail.com>
2021-02-04 22:30:30 +0100

    push

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+
 const app = express()
 
 const mongoose = require('mongoose')
@@ -15,8 +16,36 @@ app.listen(process.env.PORT,()=> console.log(`http://localhost:${process.env.POR
 app.use(express.static('public'))
 app.set('view engine','ejs')
 app.use(express.json())
+
 app.use(express.urlencoded({extended: false}))
 
+
+
+
+
 app.get('/', (req, res) => {
-    res.render('home')
-})
+    res.render('index', {  title:'nothingtotrash Home' })
+    })
+
+    app.get('/marktplatz' ,(req,res) =>{
+        res.render('marktplatz',{title:'nothingtotrash Marktplatz'})
+    })
+
+    app.get('/community' ,(req,res) =>{
+        res.render('community',{title:'nothingtotrash Community'})
+    })
+
+    app.get('/ueber' ,(req,res) =>{
+        res.render('über',{title:'nothingtotrash Über Uns'})
+    })
+
+    app.get('/resources' ,(req,res) =>{
+        res.render('resources',{title:'nothingtotrash Resources'})
+    })
+    
+    
+ 
+    
+    app.use(function (req, res, next) {
+        res.status(404).render('404', { title:'Error' });
+    });
